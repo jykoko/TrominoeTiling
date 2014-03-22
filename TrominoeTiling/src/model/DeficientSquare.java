bf06d5b2ea8227e19ee625c5b3e3ca3e042ebd96
jykoko <jykoko@stetson.edu>
2014-03-22 02:53:23 -0400

    Cleaning up GUI code and filling out some basic starter functions.

--- TrominoeTiling/src/model/DeficientSquare.java
+++ TrominoeTiling/src/model/DeficientSquare.java
@@ -2,12 +2,32 @@ package model;
 
 public class DeficientSquare 
 {
-  private int xLoc;
-  private int yLoc;
+  private int myXLoc;
+  private int myYLoc;
   
-  public DeficientSquare( int x, int y )
+  public DeficientSquare( int xCord, int yCord )
   {
-	  xLoc = x;
-	  yLoc = y;
+	  myXLoc = xCord;
+	  myYLoc = yCord;
+  }
+  
+  public void setXCoordinate( int coordinate )
+  {
+	  myXLoc = coordinate;
+  }
+  
+  public void setYCoordinate( int coordinate )
+  {
+	  myYLoc = coordinate;
+  }
+  
+  public int getXCoordinate()
+  {
+	  return myXLoc;
+  }
+  
+  public int getYCoordinate()
+  {
+	  return myYLoc;
   }
 }
