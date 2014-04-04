66019cb4b375e13fdeff704ca18533f0ce6e6a8d
jykoko <jykoko@stetson.edu>
2014-04-04 19:13:48 -0400

    Cleaning up code in model.

--- TrominoeTiling/src/model/TrominoeModel.java
+++ TrominoeTiling/src/model/TrominoeModel.java
@@ -13,7 +13,6 @@ public class TrominoeModel
 		board = new Board(size);
 		grid = board.getBoard();
 		currentNum = 1;
-		
 		defSquare = new DeficientSquare(x,y);
 		grid[defSquare.getXCoordinate()][defSquare.getYCoordinate()] = -1;
 	}
@@ -25,87 +24,73 @@ public class TrominoeModel
 	
 	private void tileRec(int size, int topx, int topy) 
 	{
-		// No recursive case needed here, just fill in your one tromino...
-		if (size == 2) {
-		
-			// Fill in the one necessary tromino. The hole is identified by a
-			// non-zero number, so don't fill in that one square.	
+		if (size == 2) 
+		{
 			for (int i=0; i<size; i++) 
+			{
 				for (int j=0; j<size; j++)
+				{
 					if (grid[topx+i][topy+j] == 0)
+					{
 						grid[topx+i][topy+j] = currentNum;
-		
-			// Advance to the next tromino.
+					}
+				}
+			}
+	
 			currentNum++;
 		}
-		
-		// Recursive case...
-		else {
-			
-			// Find coordinates of missing hole
+		else 
+		{
 			int savex=topx, savey=topy;
 			
 			for (int x=topx; x<topx+size; x++) 
+			{
 				for (int y=topy; y<topy+size; y++)
-					if (grid[x][y] != 0) {
+				{
+					if (grid[x][y] != 0) 
+					{
 						savex = x;
 						savey = y;
 					}
+				}
+			}
 				
-			// Hole in upper left quadrant.		
-			if (savex < topx + size/2 && savey < topy + size/2) {
-				
-				// Recursively tile upper left quadrant.
+			if (savex < topx + size/2 && savey < topy + size/2) 
+			{
 				tileRec(size/2, topx, topy);
 				
-				// Fill in middle tromino
 				grid[topx+size/2][topy+size/2-1] = currentNum;
 				grid[topx+size/2][topy+size/2] = currentNum;
 				grid[topx+size/2-1][topy+size/2] = currentNum;
 				
-				// Advance to the next tromino
 				currentNum++;
 				
-				// Now we can make our three other recursive calls.
 				tileRec(size/2, topx, topy+size/2);
 				tileRec(size/2, topx+size/2, topy);
-				tileRec(size/2, topx+size/2, topy+size/2);
-				
+				tileRec(size/2, topx+size/2, topy+size/2);	
 			}
-			
-			// Hole in upper right quadrant
-			else if (savex < topx + size/2 && savey >= topy + size/2) {
-				
-				// Recursively tile upper right quadrant.
+			else if (savex < topx + size/2 && savey >= topy + size/2) 
+			{
 				tileRec(size/2, topx, topy+size/2);
 				
-				// Fill in middle tromino
 				grid[topx+size/2][topy+size/2-1] = currentNum;
 				grid[topx+size/2][topy+size/2] = currentNum;
 				grid[topx+size/2-1][topy+size/2-1] = currentNum;
 				
-				// Advance to the next tromino
 				currentNum++;
 				
-				// Now we can make our three other recursive calls.
 				tileRec(size/2, topx, topy);
 				tileRec(size/2, topx+size/2, topy);
 				tileRec(size/2, topx+size/2, topy+size/2);
-				
 			}
-			
-			// Hole in bottom left quadrant
-			else if (savex >= topx + size/2 && savey < topy + size/2) {
-				
-				// Recursively tile bottom left quadrant.
+			else if (savex >= topx + size/2 && savey < topy + size/2) 
+			{
 				tileRec(size/2, topx+size/2, topy);
 				
-				// Fill in middle tromino
 				grid[topx+size/2-1][topy+size/2] = currentNum;
 				grid[topx+size/2][topy+size/2] = currentNum;
 				grid[topx+size/2-1][topy+size/2-1] = currentNum;
 				
-				// Advance to the next tromino
 				currentNum++;
 				
 				tileRec(size/2, topx, topy);
